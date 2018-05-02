85e8076863f656ba15a7df57389f0f2025fe0e18
Dongcheol Kim <dongcheol.kim@opengallery.co.kr>
2018-05-02 11:09:08 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dynamodb_to_csv',
+    version='0.0.1',
+    description='The funniest joke in the world',
+    url='https://github.com/kdc0110/django/dynamodb_to_csv',
+    author='Dongcheol Kim',
+    author_email='kdc0110@gmail.com',
+    license='MIT',
+    packages=['dynamodb_to_csv'],
+    zip_safe=False
+)
