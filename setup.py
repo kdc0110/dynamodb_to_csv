240ac45b98f79468acd1ac4a533caf15082d1db0
Dongcheol Kim <dongcheol.kim@opengallery.co.kr>
2018-05-02 11:12:28 +0900

    [Change] Change description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dynamodb_to_csv',
     version='0.0.1',
-    description='The funniest joke in the world',
+    description='dynamodb to csv package',
     url='https://github.com/kdc0110/django/dynamodb_to_csv',
     author='Dongcheol Kim',
     author_email='kdc0110@gmail.com',
